909bd9873182dacab666d789512c736e88efce48
unknown <ivansoboleuski@gmail.com>
2021-05-30 17:55:14 +0300

    Comand_linux

---
+++ test_script_1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir test_folder11 test_folder12 test_folder13
+cd test_folder1
+touch file11.txt file12.txt file13.txt file11.json file12.json
+mkdir folder14 folder15 folder16
+ls
+mv file11.txt file12.txt ~/hometask_1/test_folder1
