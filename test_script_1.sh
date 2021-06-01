0aaedd2273ad4866099b56901df3641498d321c4
jnsob <80149006+jnsob@users.noreply.github.com>
2021-06-01 23:30:18 +0300

    Delete test_script_1.sh

--- test_script_1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir test_f1 test_f2 test_f3
-cd test_f1
-touch file1.txt file2.txt file3.txt file1.json file2.json
-mkdir folder1 folder2 folder3
-ls
-mv file1.txt file2.txt folder1
-
